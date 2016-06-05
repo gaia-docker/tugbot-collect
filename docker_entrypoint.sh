c2da20a98fc8873d00f2f3024d5c58b086bc2f0c
Shay Tsadok <shayts7@gmail.com>
2016-06-05 07:36:28 +0000

    adding custom endpoint for docker to run the service under tugbot user

---
+++ docker_entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+if [ "$1" = "tugbot" ]; then
+  if [ -S /var/run/docker.sock ]; then
+    chown -R tugbot:tugbot /var/run/docker.sock
+  fi
+  exec gosu tugbot:tugbot "$@"
+fi
+
+exec "$@"
