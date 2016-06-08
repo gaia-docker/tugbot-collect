5a9af22c0a0fd883cedb300e376cd124351abcea
Shay Tsadok <shayts7@gmail.com>
2016-06-09 00:06:59 +0300

    playing around with docker api

--- main.go
+++ main.go
@@ -5,6 +5,11 @@ import (
 	"github.com/docker/engine-api/client"
 	"github.com/docker/engine-api/types"
 	"golang.org/x/net/context"
+	"github.com/vdemeester/docker-events"
+	eventtypes "github.com/docker/engine-api/types/events"
+	"os"
+	"bufio"
+	"io"
 )
 
 func main() {
@@ -14,13 +19,57 @@ func main() {
 		panic(err)
 	}
 
+	ctx, cancel := context.WithCancel(context.Background())
 	options := types.ContainerListOptions{All: true}
-	containers, err := cli.ContainerList(context.Background(), options)
+	containers, err := cli.ContainerList(ctx, options)
 	if err != nil {
 		panic(err)
 	}
 
 	for _, c := range containers {
 		fmt.Println(c.Image, "-> ", c.ID)
+		reader, pathstat, err := cli.CopyFromContainer(ctx, c.ID, "/usr/bin")
+		if err != nil {
+			fmt.Println("error from copy:", err)
+		} else {
+			// open output file
+			fo, err := os.Create("output-" + c.ID + ".tar")
+			if err != nil {
+				panic(err)
+			}
+			// close fo on exit and check for its returned error
+			defer func() {
+				if err := fo.Close(); err != nil {
+					panic(err)
+				}
+			}()
+
+			// make a buffer to keep chunks that are read
+			// buffer size==32K
+			buf := make([]byte, 32*1024)
+			writer := bufio.NewWriter(fo)
+
+			if _, err := io.CopyBuffer(writer, reader, buf); err != nil {
+				panic(err)
+			}
+
+			fmt.Println("pathstat for copied folder: ", pathstat)
+		}
+
 	}
+
+
+	fmt.Println("test docker events:")
+
+	errChan := events.Monitor(ctx, cli, types.EventsOptions{}, func(event eventtypes.Message) {
+		fmt.Printf("%v\n", event)
+	})
+
+	if err := <-errChan; err != nil {
+		// Do something
+	}
+
+
+	// Call cancel() to get out of the monitor
+	defer cancel()
 }
