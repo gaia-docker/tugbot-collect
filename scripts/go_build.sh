19afee00b48c8592147aa20b607b81294f599162
Effi <effoeffi@users.noreply.github.com>
2016-09-12 16:19:03 +0300

    Statically link 'tugbot' binary (no C runtime dependency)

--- scripts/go_build.sh
+++ scripts/go_build.sh
@@ -5,7 +5,7 @@ go_build() {
   rm -rf "${distdir}"
   mkdir "${distdir}"
   glide install
-  go build -v -o ${distdir}/tugbot-collect
+  CGO_ENABLED=0 go build -v -o ${distdir}/tugbot-collect
 }
 
 set -x
